55fbc8736843c0a6e171e373f39fb7083a0c0296
VinsDev <vinsdev185@gmail.com>
2022-09-27 14:44:16 +0100

    search

--- server.js
+++ server.js
@@ -34,12 +34,6 @@ app.get('/contact', (req, res) => {
     res.sendFile(__dirname + '/views/contact.html');
 });
 
-
-// School . . .
-app.get('/school', (req, res) => {
-    res.sendFile(__dirname + '/school/index.html');
-});
-
 app.post('/getFruits', async (req, res) => {
     let payload = req.body.payload.trim();
     let search = Fruit.find({ name: { $regex: new RegExp('^' + payload + '.*', 'i') } }).exec();
@@ -47,6 +41,14 @@ app.post('/getFruits', async (req, res) => {
     search = (await search).slice(0, 10);
     res.send({ payload: search });
 });
+
+
+// School . . .
+app.get('/school', (req, res) => {
+    res.sendFile(__dirname + '/school/index.html');
+});
+
+
 
 
 
