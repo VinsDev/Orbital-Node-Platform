a35f67d8d63c25e57fdd5e3cb66a3819c5599398
VinsDev <vinsdev185@gmail.com>
2022-09-27 11:42:04 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const Fruit = require('./liveSearch');
+const mongoose = require("mongoose");
+const { db } = require("./liveSearch");
+
+mongoose.connect(
+    "mongodb://localhost:27017/",
+    {
+        dbName: "fruitLiveSearch",
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err) =>
+        err ? console.log(err) : console.log(
+            "Connected to " + db.name)
+);
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Landing . . .
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/views/about.html');
+});
+app.get('/services', (req, res) => {
+    res.sendFile(__dirname + '/views/services.html');
+});
+app.get('/contact', (req, res) => {
+    res.sendFile(__dirname + '/views/contact.html');
+});
+
+app.post('/getFruits', async (req, res) => {
+    let payload = req.body.payload.trim();
+    let search = Fruit.find({ name: { $regex: new RegExp('^' + payload + '.*', 'i') } }).exec();
+    //Limit Search Results to 10
+    search = (await search).slice(0, 10);
+    res.send({ payload: search });
+});
+
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server has started on PORT 3000');
+});
