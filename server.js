77c59675cc564058c918d5f02710a81df33ff667
VinsDev <vinsdev185@gmail.com>
2022-09-28 23:15:14 +0100

    register stage 2

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const Fruit = require('./liveSearch');
+// const Fruit = require('./liveSearch');
 const mongoose = require("mongoose");
-const { db } = require("./liveSearch");
+const app = express();
+const server = require("http").Server(app);
+const bodyParser = require("body-parser");
 
-mongoose.connect(
+// const { db } = require("./liveSearch");
+
+/* mongoose.connect(
     "mongodb://localhost:27017/",
     {
         dbName: "fruitLiveSearch",
@@ -13,14 +17,53 @@ mongoose.connect(
     (err) =>
         err ? console.log(err) : console.log(
             "Connected to " + db.name)
+); */
+
+const username = encodeURIComponent("vins");
+const password = encodeURIComponent("vins@node");
+
+
+let uri =
+  `mongodb+srv://${username}:${password}@cluster0.1243w2d.mongodb.net/orbital?retryWrites=true&w=majority`;
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const formSchema = new mongoose.Schema(
+    {
+        data: Object,
+    },
+    { collection: "school" }
 );
 
-const app = express();
+const Form = mongoose.model("Form", formSchema);
+
+const formData = (bodyData) => {
+    Form({ data: bodyData }).save((err) => {
+        if (err) {
+            throw err;
+        }
+    });
+};
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+
+
 // Landing . . .
+app.post("/", urlencodedParser, (req, res) => {
+    formData(req.body);
+    res.render("success", { name: req.body.name });
+});
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
@@ -34,16 +77,16 @@ app.get('/contact', (req, res) => {
     res.sendFile(__dirname + '/views/contact.html');
 });
 app.get('/register', (req, res) => {
-    res.sendFile(__dirname + '/views/register.html');
+    res.render("register");
 });
 
-app.post('/getFruits', async (req, res) => {
+/* app.post('/getFruits', async (req, res) => {
     let payload = req.body.payload.trim();
     let search = Fruit.find({ name: { $regex: new RegExp('^' + payload + '.*', 'i') } }).exec();
     //Limit Search Results to 10
     search = (await search).slice(0, 10);
     res.send({ payload: search });
-});
+}); */
 
 
 // School . . .
