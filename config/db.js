3c5561d6ed69c2677f0198a638f4f7d43d9ee714
VinsDev <vinsdev185@gmail.com>
2022-10-07 11:53:24 +0100

    tr

--- config/db.js
+++ config/db.js
@@ -1,6 +1,5 @@
 let uri =
-"mongodb+srv://vins:Vins46185@cluster0.1243w2d.mongodb.net/orbital?retryWrites=true";
-
+  "mongodb+srv://vins:Vins46185@cluster0.1243w2d.mongodb.net/?retryWrites=true&w=majority";
 let url = "mongodb://localhost:27017/";
 
 
