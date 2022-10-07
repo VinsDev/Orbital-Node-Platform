fb4413053c8c74e24be8962ee0cf92a2d901f187
VinsDev <vinsdev185@gmail.com>
2022-10-07 13:53:44 +0100

    concern fixed

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 let uri =
-  "mongodb+srv://vins:Vins46185@cluster0.1243w2d.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://vins:Vins46185@cluster0.1243w2d.mongodb.net/orbital?retryWrites=true&w=majority";
 let url = "mongodb://localhost:27017/";
 
 
 module.exports = {
   url: uri,
-  database: "orbital",
+  database: "",
   imgBucket: "photos",
 };
 
