a780f3edb374e0b592b34c41cf3654fe20f3664f
VinsDev <vinsdev185@gmail.com>
2022-10-07 10:59:57 +0100

    majority problem fixed

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 let uri =
-"mongodb+srv://vins:Vins46185@cluster0.1243w2d.mongodb.net/?retryWrites=true&w=majority";
+"mongodb+srv://vins:Vins46185@cluster0.1243w2d.mongodb.net/?retryWrites=true";
 
 let url = "mongodb://localhost:27017/";
 
