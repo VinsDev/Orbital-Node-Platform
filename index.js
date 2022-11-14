a7caa7aeb6091695c01ad2f6ab66520f0172feb2
VinsDev <vinsdev185@gmail.com>
2022-11-14 13:23:04 +0100

    V

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 const path = require("path");
-const initRoutes = require("../routes");
+const initRoutes = require("./routes");
 
 var corsOptions = {
   origin: "http://localhost:3000"
@@ -12,11 +12,11 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 initRoutes(app);
 
-let port = 3000;
-app.listen(process.env.PORT || port, () => {
-  console.log(`Running at localhost:${port}`);
+let PORT = 3000;
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Running at localhost:${PORT}`);
 });
