ae9f89d7b34afcbdc64555afdfc9ac9363657e66
VinsDev <vinsdev185@gmail.com>
2022-11-14 12:17:58 +0100

    pub

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 initRoutes(app);
 
