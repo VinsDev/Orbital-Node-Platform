1d8bd14696305133911d9526dbbece153dc0966d
VinsDev <vinsdev185@gmail.com>
2023-01-09 10:29:21 +0100

    nssl

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const cors = require("cors");
-var sslRedirect = require('heroku-ssl-redirect');
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -14,8 +13,6 @@ app.use(express.json());
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(sslRedirect());
 
 initRoutes(app);
 
