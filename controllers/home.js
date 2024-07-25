435d9dbf98a3742429f5480f9a0781bcc64c100c
Vincent Dominic <113354272+VinsDev@users.noreply.github.com>
2024-07-25 10:47:21 +0100

    Update home.js

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 const first = (req, res) => {
-  return res.redirect('https://orbital-node-platform.onrender.com/home');
+  return res.redirect('https://orbital-node-website.onrender.com/home');
 };
 const home = (req, res) => {
   return res.sendFile(path.join(`${__dirname}/../views/index.html`));
